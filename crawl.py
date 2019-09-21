b357addbaae65cffc0e8f8bef498c197d87783f0
confiself <1120396510@qq.com>
2019-09-21 20:55:26 +0800

    crawl messages

--- crawl.py
+++ crawl.py
@@ -3,84 +3,122 @@
 
 import requests
 import time
-
-headers = {
-    'Host': 'm.weibo.cn',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Referer': 'https://m.weibo.cn/u/5175429989'
-}
+import os
 
 
-def get_page(page):
-    '''请求url，并获取内容
-    :param page:
-    :return:
-    '''
-    params = {
-        'type': 'uid',
-        'value': '5175429989',
-        'containerid': '1076035175429989',
-        'page': page
+class CrawlComments(object):
 
+    headers = {
+        'Host': 'm.weibo.cn',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Referer': 'https://m.weibo.cn/u/5175429989'
     }
-    url = 'https://m.weibo.cn/api/container/getIndex'
-    try:
-        res = requests.get(url, headers=headers, params=params)
-        if res.status_code == 200:
-            return res.json()
-    except requests.ConnectionError as e:
-        print('Error', e.args)
 
+    def __init__(self, uid, container_id):
+        self.uid = uid
+        self.container_id = container_id
+        self.comments_finished = set()
+        self.comments_file = '/opt/app/data/wei_bo_comments.txt'
+        self._load()
 
-def parse_page(json, page):
-    if json:
-        items = json.get('data').get('cards')
-        for index, item in enumerate(items):
+    def _load(self):
+        if not os.path.exists(self.comments_file):
+            return
+        with open(self.comments_file) as f:
+            self.comments_finished = set(map(lambda x: x.strip(), f.readlines()))
+
+    def crawl(self):
+        page = 1
+        while page != -1:
+            page_key = 'page,{}'.format(page)
+            if page_key in self.comments_finished:
+                page += 1
+                continue
+            contents, page = self.get_page(page)
+            if not contents:
+                continue
+            self.comments_finished.add(page_key)
+            for content in contents:
+                comments = self.get_comments(content['id'])
+                self.save_comments(comments)
+            time.sleep(1)
+
+    def save_comments(self, comments):
+        with open(self.comments_file, 'a') as f:
+            for text, comment_id in comments:
+                comment_key = 'comment_id,{}'
+                if comment_key in self.comments_finished:
+                    continue
+                self.comments_finished.add(comment_key)
+                f.writelines(text + '\n')
+
+    def get_page(self, page):
+        '''请求url，并获取内容
+        :param page:
+        :return:
+        '''
+        params = {
+            'type': 'uid',
+            'value': self.uid,
+            'containerid': self.container_id,
+            'page': page
+
+        }
+        url = 'https://m.weibo.cn/api/container/getIndex'
+        try:
+            res = requests.get(url, headers=self.headers, params=params)
+            if res.status_code == 200 and 'data' in res.json():
+                # finish detected
+                if 'cards' not in res.json()['data'] or not res.json()['data']['cards']:
+                    return None, -1
+                cards = res.json()['data']['cards']
+                return self.parse_page(cards, page), page + 1
+        except requests.ConnectionError as e:
+            print('Error', e.args)
+        return None, page + 1
+
+    @staticmethod
+    def parse_page(cards, page):
+        for index, item in enumerate(cards):
+            if 'mblog' not in item:
+                continue
             if page == 1 and index == 1:
                 continue
             else:
                 item = item.get('mblog')
-                weibo = {}
-                weibo['id'] = item.get('id')
+                weibo = {'id': item.get('id'),
+                         'attitudes': item.get('attitudes_count'),
+                         'comments': item.get('comments_count'),
+                         'reposts': item.get('reposts_count')}
                 # weibo['text'] = pq(item.get('text')).text()
-                weibo['attitudes'] = item.get('attitudes_count')
-                weibo['comments'] = item.get('comments_count')
-                weibo['reposts'] = item.get('reposts_count')
                 yield weibo
 
-
-def get_comments(wb_id):
-    Data = []
-    url = 'https://m.weibo.cn/api/comments/show?id={id}'.format(id=wb_id)
-    page_url = 'https://m.weibo.cn/api/comments/show?id={id}&page={page}'
-    Resp = requests.get(url, headers=headers)
-    page_max_num = Resp.json()['data']['max']
-    for i in range(page_max_num):
-        p_url = page_url.format(id=wb_id, page=i + 1)
-        resp = requests.get(p_url, headers=headers)
-        resp_data = resp.json()['data']
-        data = resp_data['data']
-        for d in data:
-            review_id = d['id']
-            like_counts = d['like_counts']
-            source = d['source']
-            username = d['user']['screen_name']
-            image = d['user']['profile_image_url']
-            verified = d['user']['verified']
-            verified_type = d['user']['verified_type']
-            profile_url = d['user']['profile_url']
-            comment = d['text']
-            print(d)
+    def get_comments(self, wb_id):
+        url = 'https://m.weibo.cn/api/comments/show?id={id}'.format(id=wb_id)
+        page_url = 'https://m.weibo.cn/api/comments/show?id={id}&page={page}'
+        Resp = requests.get(url, headers=self.headers)
+        page_max_num = Resp.json()['data']['max']
+        for i in range(page_max_num):
+            p_url = page_url.format(id=wb_id, page=i + 1)
+            resp = requests.get(p_url, headers=self.headers)
+            resp_data = resp.json()['data']
+            data = resp_data['data']
+            for d in data:
+                review_id = d['id']
+                like_counts = d['like_counts']
+                source = d['source']
+                username = d['user']['screen_name']
+                image = d['user']['profile_image_url']
+                verified = d['user']['verified']
+                verified_type = d['user']['verified_type']
+                profile_url = d['user']['profile_url']
+                comment = d['text']
+                yield comment
             time.sleep(1)
 
 
 if __name__ == '__main__':
-    for page in range(1, 11):
-        json = get_page(page)
-        results = parse_page(json, page)
-        for result in results:
-            print(result)
-            #
-            time.sleep(1)
-    get_comments('4417893804039537')
+    _comments = CrawlComments('5175429989', '1076035175429989')
+    _comments.crawl()
