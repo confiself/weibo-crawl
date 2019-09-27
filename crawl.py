c766929a3f358041fc0acaff194ecaa10217356e
confiself <1120396510@qq.com>
2019-09-27 18:53:33 +0800

    crawl messages

--- crawl.py
+++ crawl.py
@@ -4,30 +4,43 @@
 import requests
 import time
 import os
+import json
 
 
 class CrawlComments(object):
-
+    """
+    需要不断更换cookie
+    参考微博爬虫，自己实现
+    """
     headers = {
         'Host': 'm.weibo.cn',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
         'X-Requested-With': 'XMLHttpRequest',
-        'Referer': 'https://m.weibo.cn/u/5175429989'
+        'Referer': 'https://m.weibo.cn/u/5175429989',
+        'Cookie': '_T_WM=76414085534; SUB=_2A25wiPo7DeRhGedI6VUX8ifJyj-IHXVQcoZzrDV6PUJbktAKLWekkW1NV74_lxJ-Qtw5-OEZgWgsddE9ho-xPG6O; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5wUH6RDWfxkGin5-qfbUWT5JpX5KzhUgL.Fo2ceoMceo.feKe2dJLoIEBLxKqL12-LBKMLxKBLBonL1KqLxKqLBo-LB--LxKqLBozL1h.t; SUHB=0D-0bw_siFdpnC; SSOLoginState=1569491563; MLOGIN=1; XSRF-TOKEN=1d872b; WEIBOCN_FROM=1110006030; M_WEIBOCN_PARAMS=lfid%3D100803%26luicode%3D20000174%26uicode%3D20000174'
     }
 
     def __init__(self, uid, container_id):
         self.uid = uid
         self.container_id = container_id
         self.comments_finished = set()
+        self.comments_finished_file = '/opt/app/data/wei_bo_finished_info.txt'
         self.comments_file = '/opt/app/data/wei_bo_comments.txt'
+        self.sess = requests.session()
+        self.sess.headers = self.headers
         self._load()
 
     def _load(self):
-        if not os.path.exists(self.comments_file):
+        if not os.path.exists(self.comments_finished_file):
             return
-        with open(self.comments_file) as f:
+        with open(self.comments_finished_file) as f:
             self.comments_finished = set(map(lambda x: x.strip(), f.readlines()))
+
+    def _add_page(self, page_key):
+        self.comments_finished.add(page_key)
+        with open(self.comments_finished_file, 'a') as f_finished:
+            f_finished.writelines(page_key + '\n')
 
     def crawl(self):
         page = 1
@@ -39,20 +52,24 @@ class CrawlComments(object):
             contents, page = self.get_page(page)
             if not contents:
                 continue
-            self.comments_finished.add(page_key)
+            self._add_page(page_key)
             for content in contents:
                 comments = self.get_comments(content['id'])
                 self.save_comments(comments)
             time.sleep(1)
 
     def save_comments(self, comments):
-        with open(self.comments_file, 'a') as f:
-            for text, comment_id in comments:
-                comment_key = 'comment_id,{}'
+        with open(self.comments_finished_file, 'a') as f_finished, \
+                open(self.comments_file, 'a') as f_comments:
+            for comment in comments:
+                comment_key = 'comment_id,{}'.format(comment['id'])
                 if comment_key in self.comments_finished:
                     continue
                 self.comments_finished.add(comment_key)
-                f.writelines(text + '\n')
+                f_finished.writelines(comment_key + '\n')
+                comment = json.dumps(comment, ensure_ascii=False)
+                f_comments.writelines(comment + '\n')
+                print('{} finished, add comment {}'.format(len(self.comments_finished), comment))
 
     def get_page(self, page):
         '''请求url，并获取内容
@@ -68,7 +85,7 @@ class CrawlComments(object):
         }
         url = 'https://m.weibo.cn/api/container/getIndex'
         try:
-            res = requests.get(url, headers=self.headers, params=params)
+            res = self.sess.get(url, headers=self.headers, params=params)
             if res.status_code == 200 and 'data' in res.json():
                 # finish detected
                 if 'cards' not in res.json()['data'] or not res.json()['data']['cards']:
@@ -98,26 +115,31 @@ class CrawlComments(object):
     def get_comments(self, wb_id):
         url = 'https://m.weibo.cn/api/comments/show?id={id}'.format(id=wb_id)
         page_url = 'https://m.weibo.cn/api/comments/show?id={id}&page={page}'
-        Resp = requests.get(url, headers=self.headers)
-        page_max_num = Resp.json()['data']['max']
+        resp = self.sess.get(url, headers=self.headers)
+        if 'data' not in resp.json()['data']:
+            return []
+        page_max_num = resp.json()['data']['max']
+        comments = []
         for i in range(page_max_num):
             p_url = page_url.format(id=wb_id, page=i + 1)
-            resp = requests.get(p_url, headers=self.headers)
+            resp = self.sess.get(p_url, headers=self.headers)
+            if 'data' not in resp.json():
+                print(resp.json())
+                continue
             resp_data = resp.json()['data']
             data = resp_data['data']
             for d in data:
-                review_id = d['id']
-                like_counts = d['like_counts']
-                source = d['source']
-                username = d['user']['screen_name']
-                image = d['user']['profile_image_url']
-                verified = d['user']['verified']
-                verified_type = d['user']['verified_type']
-                profile_url = d['user']['profile_url']
-                comment = d['text']
-                yield comment
-            time.sleep(1)
+                if 'reply_id' not in d:
+                    continue
+                _comment = {'id': d['id'],
+                            'reply_id': d['reply_id'],
+                            'text': d['text'],
+                            'reply_text': d['reply_text'],
+                            'user_name': d['user']['screen_name']
+                            }
+                comments.append(_comment)
 
+        return comments
 
 if __name__ == '__main__':
     _comments = CrawlComments('5175429989', '1076035175429989')
